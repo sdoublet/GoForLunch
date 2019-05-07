591f995fb4f06540ad9a52eecb19b913b7f42573
sdoublet <doubletsebastien@sfr.fr>
2019-05-07 10:55:14 +0200

    set authentification method with google, facebook and email

--- app/src/main/java/com/example/goforlunch/controler/activities/BaseActivity.java
+++ app/src/main/java/com/example/goforlunch/controler/activities/BaseActivity.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import butterknife.ButterKnife;
+
+
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        this.setContentView(this.getFragmentLayout());
+        ButterKnife.bind(this);
     }
+
+    public abstract int getFragmentLayout();
 }
+
+
--- app/src/main/java/com/example/goforlunch/controler/activities/MainActivity.java
+++ app/src/main/java/com/example/goforlunch/controler/activities/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.goforlunch.controler.activities;
 
 
-import android.os.Bundle;
+
 
 import com.example.goforlunch.R;
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
+import butterknife.OnClick;
+
 
 public class MainActivity extends BaseActivity {
 
+    private static final int RC_SIGN_IN = 123;
+
+
+
+    private void startSignInActivity(){
+        startActivityForResult(AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setTheme(R.style.LoginTheme)
+        .setAvailableProviders(
+                Arrays.asList(new AuthUI.IdpConfig.GoogleBuilder().build(),
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.FacebookBuilder().build())
+        )
+                .setLogo(R.drawable.avatar2)
+        .setIsSmartLockEnabled(false, true)
+        .build(),
+                RC_SIGN_IN);
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getFragmentLayout() {
+        return R.layout.activity_main;
+    }
+    @OnClick(R.id.button_connection_main_activity)
+    public void onClickButtonConnection(){
+        startSignInActivity();
     }
 }
