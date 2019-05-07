99a996ecb9f5835e015bdf4aede45aa0acd04682
sdoublet <doubletsebastien@sfr.fr>
2019-05-07 17:47:19 +0200

    start mainAtivity if user logged

--- app/src/main/java/com/example/goforlunch/controler/activities/LoginActivity.java
+++ app/src/main/java/com/example/goforlunch/controler/activities/LoginActivity.java
@@ -1,17 +1,22 @@
 package com.example.goforlunch.controler.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.goforlunch.R;
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,13 +26,32 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        if (isCurrentUserLogged()){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else {
+            this.startSignInActivity();
+        }
     }
 
     private static final int RC_SIGN_IN = 123;
 
+    //------------------
+    //UTILS
+    //------------------
+    protected FirebaseUser getCurrentUser(){
+        return FirebaseAuth.getInstance().getCurrentUser();
+    }
+    protected Boolean isCurrentUserLogged(){
+        return (this.getCurrentUser()!=null);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponseAfterSignIn(requestCode, resultCode, data);
+    }
 
-
-    private void startSignInActivity(){
+    private void startSignInActivity() {
         startActivityForResult(AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setTheme(R.style.LoginTheme)
@@ -41,10 +65,22 @@ public class LoginActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN);
     }
+    //show snack bar with a message
 
-
-    @OnClick(R.id.button_connection_main_activity)
-    public void onClickButtonConnection(){
-        startSignInActivity();
+    private void handleResponseAfterSignIn(int requestCode, int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Authentification réussie", Toast.LENGTH_LONG).show();
+                // TODO: 07/05/2019   mettre intent vers mainactivity
+            } else if (response == null) {
+                Toast.makeText(this, "Authentificaiton annulée", Toast.LENGTH_LONG).show();
+            } else if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                Toast.makeText(this, "Aucune connexion internet", Toast.LENGTH_LONG).show();
+            } else if (response.getError().getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                Toast.makeText(this, "Une érreur s'est produite", Toast.LENGTH_LONG).show();
+            }
+        }
     }
+
 }
