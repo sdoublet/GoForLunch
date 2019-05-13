c63897786df86c629d0d864aa511913a2c071724
sdoublet <doubletsebastien@sfr.fr>
2019-05-13 10:22:42 +0200

    add row_resto_list  - recyclerviews and divider

---
+++ app/src/main/java/com/example/goforlunch/views/recyclerViews/RecyclerviewHolder.java
@@ -0,0 +1,39 @@
+package com.example.goforlunch.views.recyclerViews;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.goforlunch.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecyclerviewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.resto_name)
+    TextView restoName;
+    @BindView(R.id.resto_kind)
+    TextView restoKind;
+    @BindView(R.id.resto_address)
+    TextView restoAddress;
+    @BindView(R.id.resto_opening_time)
+    TextView restoOpeningTime;
+    @BindView(R.id.resto_distance)
+    TextView restoDistance;
+    @BindView(R.id.person_selected)
+    TextView personSelected;
+    @BindView(R.id.rating)
+    RatingBar rating;
+    @BindView(R.id.resto_photo)
+    ImageView restoPhoto;
+
+    public RecyclerviewHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this, itemView);
+    }
+}
