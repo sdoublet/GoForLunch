e8cda8a5fa778aa0e48f5c479c4f2e1f387c424f
sdoublet <doubletsebastien@sfr.fr>
2019-05-24 18:03:59 +0200

    set PlaceStream with all streamFetches

--- app/src/main/java/com/example/goforlunch/utils/PlaceStreams.java
+++ app/src/main/java/com/example/goforlunch/utils/PlaceStreams.java
@@ -1,8 +1,40 @@
 package com.example.goforlunch.utils;
 
 
+import com.example.goforlunch.model.Api.Autocomplete.Prediction;
+import com.example.goforlunch.model.Api.Details.PlaceDetail;
+import com.example.goforlunch.model.Api.Nearby.NearbyPlaces;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class PlaceStreams {
+
+
+    private static  PlaceServices placeservices = PlaceServices.retrofit.create(PlaceServices.class);
+
+    public static Observable<NearbyPlaces> streamFetchNearbySearch(String location, int radius, String type, String key ){
+        return placeservices.getNearbyPlaces(location, radius, type, key)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .timeout(10, TimeUnit.SECONDS);
+    }
+
+    public static Observable<PlaceDetail> streamFetchPlaceDetails(String placeId, String key){
+        return placeservices.getPlaceDetails(placeId, key)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .timeout(10, TimeUnit.SECONDS);
+    }
+
+    public static Observable<Prediction> streamFetchPrediction(String input, String location, int radius, String key){
+        return placeservices.getPrediction(input, location, radius, key)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .timeout(10, TimeUnit.SECONDS);
+    }
 
 }
