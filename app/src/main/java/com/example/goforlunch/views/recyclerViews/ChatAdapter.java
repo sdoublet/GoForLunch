0183f5e85f2ec6c5a622767290e2167fb7629b05
sdoublet <doubletsebastien@sfr.fr>
2019-05-28 17:16:32 +0200

    set ChatAdapter

--- app/src/main/java/com/example/goforlunch/views/recyclerViews/ChatAdapter.java
+++ app/src/main/java/com/example/goforlunch/views/recyclerViews/ChatAdapter.java
@@ -1,4 +1,51 @@
 package com.example.goforlunch.views.recyclerViews;
 
-public class ChatAdapter {
+import android.net.sip.SipSession;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.RequestManager;
+import com.example.goforlunch.R;
+import com.example.goforlunch.model.Message;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+
+public class ChatAdapter extends FirestoreRecyclerAdapter<Message, MessageViewHolder> {
+
+    public interface Listener{
+        void onDataChanged();
+    }
+    //FOR DATA
+    private final RequestManager glide;
+    private final String idCurrentUser;
+
+    //FOR COMMUNICATION
+   private Listener callback;
+
+    public ChatAdapter(@NonNull FirestoreRecyclerOptions<Message> options, RequestManager glide, String idCurrentUser) {
+        super(options);
+        this.glide = glide;
+        this.idCurrentUser = idCurrentUser;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull MessageViewHolder messageViewHolder, int i, @NonNull Message message) {
+        messageViewHolder.updateWithMessage(message, this.idCurrentUser, this.glide);
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MessageViewHolder(LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.fragment_chat, parent,false  ));
+    }
+
+    @Override
+    public void onDataChanged() {
+        super.onDataChanged();
+        this.callback.onDataChanged();
+    }
 }
