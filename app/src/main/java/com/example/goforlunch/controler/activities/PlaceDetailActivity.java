bae10a3f3bb055d257a3901a96edee0c4ec60f90
sdoublet <doubletsebastien@sfr.fr>
2019-06-06 14:59:22 +0200

    add rating result in PlaceDetailActivity

--- app/src/main/java/com/example/goforlunch/controler/activities/PlaceDetailActivity.java
+++ app/src/main/java/com/example/goforlunch/controler/activities/PlaceDetailActivity.java
@@ -5,11 +5,9 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 
 import com.bumptech.glide.Glide;
 import com.example.goforlunch.BuildConfig;
@@ -27,17 +25,20 @@ import io.reactivex.observers.DisposableObserver;
 
 public class PlaceDetailActivity extends BaseActivity {
 
-    public static final String PLACEDETAILRESTO= "resto_place_id";
+    public static final String PLACEDETAILRESTO = "resto_place_id";
     public static final String PHOTO = "photo";
-   // @BindView(R.id.toolbar)
-   // Toolbar toolbar;
+    // @BindView(R.id.toolbar)
+    // Toolbar toolbar;
     @BindView(R.id.floating_button)
     FloatingActionButton floatingButton;
     @BindView(R.id.detail_resto_name)
     TextView restoName;
-    @BindView(R.id.detail_address)TextView restoAddress;
+    @BindView(R.id.detail_address)
+    TextView restoAddress;
     @BindView(R.id.detail_image_resto)
     ImageView photoResto;
+    @BindView(R.id.rating)
+    RatingBar restoRating;
     private String restoPlaceId;
     private Disposable disposable;
     private Result placeDetailResult;
@@ -46,13 +47,13 @@ public class PlaceDetailActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // setContentView(R.layout.activity_place_detail);
+        // setContentView(R.layout.activity_place_detail);
         ButterKnife.bind(this);
         restoPlaceId = getIntent().getStringExtra(PLACEDETAILRESTO);
         Log.e("test", restoPlaceId);
         executeHttpRequestWithRetrofit(restoPlaceId);
 
-       // photo = getIntent().getStringExtra("photo");
+        // photo = getIntent().getStringExtra("photo");
 
 
     }
@@ -63,14 +64,14 @@ public class PlaceDetailActivity extends BaseActivity {
     }
 
     @OnClick(R.id.floating_button)
-    public void OnclickFloatingActionButton(){
-        if (!floatingButton.isActivated()){
+    public void OnclickFloatingActionButton() {
+        if (!floatingButton.isActivated()) {
             Drawable drawable = getResources().getDrawable(R.drawable.check_circle).mutate();
             drawable.setColorFilter(getResources().getColor(R.color.green), PorterDuff.Mode.SRC_ATOP);
             Toast.makeText(getBaseContext(), "You choose this restaurant", Toast.LENGTH_SHORT).show();
             floatingButton.setImageDrawable(drawable);
             floatingButton.setActivated(true);
-        }else {
+        } else {
             Drawable drawable = getResources().getDrawable(R.drawable.add_circle).mutate();
             Toast.makeText(getBaseContext(), "Make your choice", Toast.LENGTH_SHORT).show();
             floatingButton.setImageDrawable(drawable);
@@ -83,7 +84,7 @@ public class PlaceDetailActivity extends BaseActivity {
     //-----------------------
     //HTTP REQUEST
     //-----------------------
-    public void executeHttpRequestWithRetrofit(String placeId){
+    public void executeHttpRequestWithRetrofit(String placeId) {
         this.disposable = PlaceStreams.streamFetchPlaceDetails(placeId, BuildConfig.GOOGLE_MAPS_API_KEY).subscribeWith(newObserver());
 
     }
@@ -92,16 +93,16 @@ public class PlaceDetailActivity extends BaseActivity {
         return new DisposableObserver<T>() {
             @Override
             public void onNext(T t) {
-                if (t instanceof PlaceDetail){
+                if (t instanceof PlaceDetail) {
                     placeDetailResult = ((PlaceDetail) t).getResult();
-                    updateUI((PlaceDetail)t);
-                }else
-                    Log.e("TAG", "disposableObserver onNext" +t.getClass());
+                    updateUI((PlaceDetail) t);
+                } else
+                    Log.e("TAG", "disposableObserver onNext" + t.getClass());
             }
 
             @Override
             public void onError(Throwable e) {
-onFailureListener();
+                onFailureListener();
             }
 
             @Override
@@ -114,18 +115,34 @@ onFailureListener();
     //------------------
     //UPDATE UI
     //------------------
-    private void updateUI(PlaceDetail results){
-        if (results.getResult()!=null){
-          restoName.setText(results.getResult().getName());
-          restoAddress.setText(results.getResult().getFormattedAddress());
-          if (results.getResult().getPhotos()!=null){
-              String restoPhoto = results.getResult().getPhotos().get(0).getPhotoReference();
-              String photoUrl ="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + restoPhoto + "&key="+ BuildConfig.GOOGLE_MAPS_API_KEY;
-              Glide.with(getBaseContext()).load(photoUrl).into(photoResto);
-          }else {
-              photoResto.setImageResource(R.drawable.serveur);
-          }
-        }else
+    private void updateUI(PlaceDetail results) {
+        if (results.getResult() != null) {
+
+            //-------------------------
+            //Display restaurant info
+            //-------------------------
+            restoName.setText(results.getResult().getName());
+            restoAddress.setText(results.getResult().getFormattedAddress());
+            //-------------------------
+            //Display restaurant photo
+            //-------------------------
+            if (results.getResult().getPhotos() != null) {
+                String restoPhoto = results.getResult().getPhotos().get(0).getPhotoReference();
+                String photoUrl = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + restoPhoto + "&key=" + BuildConfig.GOOGLE_MAPS_API_KEY;
+                Glide.with(getBaseContext()).load(photoUrl).into(photoResto);
+            } else {
+                photoResto.setImageResource(R.drawable.serveur);
+            }
+            //--------------------------
+            //Display restaurant rating
+            //--------------------------
+            double rating = results.getResult().getRating();
+            double ratingResult = (rating/5)*3;
+            restoRating.setRating((float) ratingResult);
+            Log.e("rating", String.valueOf(rating));
+            Log.e("rating", String.valueOf(ratingResult));
+
+        } else
             restoName.setText("No name");
     }
 
