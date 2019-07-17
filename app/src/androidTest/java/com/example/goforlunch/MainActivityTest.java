fa06bd9df36bbd3309609e49d8cb9db3298d0cdc
sdoublet <doubletsebastien@sfr.fr>
2019-07-17 09:22:41 +0200

    add jacocco dependencies and create new tests

---
+++ app/src/androidTest/java/com/example/goforlunch/MainActivityTest.java
@@ -0,0 +1,45 @@
+package com.example.goforlunch;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static org.hamcrest.core.IsNot.not;
+
+import com.example.goforlunch.controler.activities.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void navigationDrawerOpen(){
+       onView(withContentDescription(R.string.navigation_drawer_open)).perform(click());
+       onView(withId(R.id.nav_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void navigaitonDrawerClose(){
+        onView(withContentDescription(R.string.navigation_drawer_open)).perform(click());
+        Espresso.pressBack();
+        onView(withId(R.id.nav_view)).check(matches((not(isDisplayed()))));
+    }
+
+    @Test
+    public void navigationClickItem(){
+        onView(withId(R.id.drawer_layout_activity_main)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.setting));
+    }
+}
